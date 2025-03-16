cc10dc0c962d3f6bf28b44cd281968a5f193bd43
lng205 <lng205.yb@gmail.com>
2025-03-16 23:59:54 +0800

    add gemini

--- src/main/java/com/lng205/memetagger/MemeTaggerApplication.java
+++ src/main/java/com/lng205/memetagger/MemeTaggerApplication.java
@@ -1,6 +1,8 @@
 package com.lng205.memetagger;
 
-import org.apache.ibatis.annotations.Insert;
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +15,21 @@ public abstract class MemeTaggerApplication {
         SpringApplication.run(MemeTaggerApplication.class, args);
     }
 
+    @Value("${key.gemini}")
+    private String key;
+
     @Bean
     public CommandLineRunner commandLineRunner() {
-        return args -> System.out.println("Hello World");
+        return args -> {
+            System.out.println("Starting MemeTagger application...");
+
+            System.out.println("Key: " + key);
+            Client client = Client.builder().apiKey(key).build();
+            GenerateContentResponse response = client.models.generateContent("gemini-2.0-flash",
+                    "What is the meaning of life?", null);
+            System.out.println(response.text());
+
+            System.out.println("Finished MemeTagger application...");
+        };
     }
 }
