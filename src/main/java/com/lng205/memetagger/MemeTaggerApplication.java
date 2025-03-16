b1cffaad85f68c8c25419f4a3489499c8b9054f3
lng205 <lng205.yb@gmail.com>
2025-03-16 22:29:18 +0800

    init

---
+++ src/main/java/com/lng205/memetagger/MemeTaggerApplication.java
@@ -0,0 +1,13 @@
+package com.lng205.memetagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeTaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeTaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lng205/memetagger/MemeTaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lng205.memetagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeTaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
